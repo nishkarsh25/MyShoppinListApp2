4b920ec24e47a5e50ca169ff7315c4a38199ae70
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-03-01 21:31:56 +0530

    Finalizing Retrofit elements

--- app/src/main/java/eu/tutorials/myshoppinlistapp2/LocationViewModel.kt
+++ app/src/main/java/eu/tutorials/myshoppinlistapp2/LocationViewModel.kt
@@ -1,14 +1,39 @@
-package eu.tutorials.myshoppinlistapp2
+package eu.tutorials.myshoppinglistapp
 
+import android.util.Log
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import eu.tutorials.myshoppinlistapp2.GeocodingResult
+import eu.tutorials.myshoppinlistapp2.LocationData
+import eu.tutorials.myshoppinlistapp2.RetrofitClient
+import kotlinx.coroutines.launch
 
 class LocationViewModel: ViewModel() {
-    private val _location = mutableStateOf<LocationData?>(null)
-    val location: State<LocationData?> = _location
+    private  val _location= mutableStateOf<LocationData?>(null)
+    val location : State<LocationData?> = _location
 
-    fun updateLocation(newLocation: LocationData) {
+    private val _address = mutableStateOf(listOf<GeocodingResult>())
+    val address: State<List<GeocodingResult>> = _address
+
+
+    fun updateLocation(newLocation: LocationData){
         _location.value = newLocation
     }
+
+    fun fetchAddress(latlng: String){
+        try{
+            viewModelScope.launch {
+                val result = RetrofitClient.create().getAddressFromCoordinates(
+                    latlng,
+                    "AIzaSyCQCkW3FprEmr10IaDtwoe_dl-y273Qy1c"
+                )
+                _address.value = result.results
+            }
+        }catch(e:Exception) {
+            Log.d("res1", "${e.cause} ${e.message}")
+        }
+    }
+
 }
