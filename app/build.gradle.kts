5f26bda12b760dff6d0156c77032f0d00b3279a7
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-03-01 06:42:52 +0530

    Setting up the API Key for Google Maps and also internet permission for the app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("androidx.navigation:navigation-compose:2.7.4")
-    
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
